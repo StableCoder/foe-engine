0f3021eeb5ea35752d336c1c39a9972099e461a1
George Cave <gcave@stablecoder.ca>
2020-12-14 01:23:48 -0500

    Basic IcoSphere model generator
    Both vertex and index data, except for colour information.

---
+++ libs/foe_model/include/foe/model/ico_sphere.hpp
@@ -0,0 +1,80 @@
+/*
+    Copyright (C) 2020 George Cave.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef FOE_MODEL_ICO_SPHERE_HPP
+#define FOE_MODEL_ICO_SPHERE_HPP
+
+#include <foe/model/vertex_component.hpp>
+
+#include <cstdint>
+
+/**
+ * @brief Returns the number of vertices for the cube mesh
+ * @param recursion Number of times to subdivide the cube
+ * @return Number of unique vertices worth of data
+ * @note Each level of recursion increases the smoothness of the cube, at the expense of
+ * exponential number of vertices.
+ */
+FOE_MODEL_EXPORT int foeModelIcoSphereNumVertices(int recursion) noexcept;
+
+/**
+ * @brief Copies requested data components into the provided buffer
+ * @param recursion Number of times to subdivide the cube
+ * @param componentCount Number of vertex components requested
+ * @param pComponents Array of component types to copy into the provided buffer, contiguously
+ * @param pData[out] Buffer to copy the requested data into. Must be minimum of
+ * `foeModelIcoSphereNumVertices() * foeGetVertexComponentStride(componentCount,pComponents) *
+ * sizeof(float)` bytes.
+ * @note Each level of recursion increases the smoothness of the cube, at the expense of
+ * exponential number of vertices.
+ */
+FOE_MODEL_EXPORT void foeModelIcoSphereVertexData(int recursion,
+                                                  uint32_t componentCount,
+                                                  foeVertexComponent const *pComponents,
+                                                  float *pData);
+
+/**
+ * @brief Returns the number of indices for the cube mesh
+ * @param recursion Number of times to subdivide the cube
+ * @return Number of indices
+ * @note Each level of recursion increases the smoothness of the cube, at the expense of
+ * exponential number of vertices.
+ */
+FOE_MODEL_EXPORT int foeModelIcoSphereNumIndices(int recursion) noexcept;
+
+/**
+ * @brief Copies cube 16-bit index data into the provided buffer
+ * @param recursion Number of times to subdivide the cube
+ * @param offset Offset to add to the returned data
+ * @param pData[out] Buffer to copy indice data to. Must be minimum of
+ * `foeModelIcoSphereNumIndices() * sizeof(uint16_t)` bytes.
+ * @note Each level of recursion increases the smoothness of the cube, at the expense of
+ * exponential number of vertices.
+ */
+FOE_MODEL_EXPORT void foeModelIcoSphereIndexData16(int recursion, uint16_t offset, uint16_t *pData);
+
+/**
+ * @brief Copies cube 32-bit index data into the provided buffer
+ * @param recursion Number of times to subdivide the cube
+ * @param offset Offset to add to the returned data
+ * @param pData[out] Buffer to copy indice data to. Must be minimum of
+ * `foeModelIcoSphereNumIndices() * sizeof(uint32_t)` bytes.
+ * @note Each level of recursion increases the smoothness of the cube, at the expense of
+ * exponential number of vertices.
+ */
+FOE_MODEL_EXPORT void foeModelIcoSphereIndexData32(int recursion, uint32_t offset, uint32_t *pData);
+
+#endif // FOE_MODEL_ICO_SPHERE_HPP
---
+++ libs/foe_model/src/ico_sphere.cpp
@@ -0,0 +1,248 @@
+/*
+    Copyright (C) 2020 George Cave.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <foe/model/ico_sphere.hpp>
+
+#include <glm/ext.hpp>
+#include <glm/glm.hpp>
+
+#include "model_log.hpp"
+
+#include <cmath>
+#include <map>
+#include <tuple>
+
+namespace {
+
+// IcoSphere code derived from origin by `Andreas Kahler` @
+// http://blog.andreaskahler.com/2009/06/creating-icosphere-mesh-in-code.html
+
+uint32_t getMiddlePoint(uint32_t index1,
+                        uint32_t index2,
+                        std::map<std::tuple<uint32_t, uint32_t>, uint32_t> &middlePointCache,
+                        std::vector<glm::vec3> &vertices) {
+    // Check cache
+    auto searchIt = middlePointCache.find(std::make_tuple(index1, index2));
+    if (searchIt != middlePointCache.end()) {
+        return searchIt->second;
+    }
+    searchIt = middlePointCache.find(std::make_tuple(index2, index1));
+    if (searchIt != middlePointCache.end()) {
+        return searchIt->second;
+    }
+
+    // Generate new point
+    glm::vec3 point1 = vertices[index1];
+    glm::vec3 point2 = vertices[index2];
+
+    glm::vec3 middle = point1 + point2;
+    middle /= 2.0;
+
+    uint32_t newIndex = vertices.size();
+    vertices.emplace_back(glm::normalize(middle));
+    middlePointCache[std::make_tuple(index1, index2)] = newIndex;
+    return newIndex;
+}
+
+glm::vec3 calculateTangent(glm::vec3 const &normal) {
+    glm::vec3 c1 = glm::cross(normal, glm::vec3{0, 0, 1});
+    glm::vec3 c2 = glm::cross(normal, glm::vec3{0, 1, 0});
+
+    if (glm::length(c1) > glm::length(c2)) {
+        return glm::normalize(c1);
+    } else {
+        return glm::normalize(c2);
+    }
+}
+
+glm::vec3 calculateBitangent(glm::vec3 const &normal) {
+    return glm::normalize(glm::cross(normal, calculateTangent(normal)));
+}
+
+auto createIcoSphere(int recursion) -> std::tuple<std::vector<glm::vec3>, std::vector<glm::uvec3>> {
+    std::vector<glm::vec3> vertices;
+    std::vector<glm::uvec3> faces;
+
+    std::map<std::tuple<uint32_t, uint32_t>, uint32_t> middlePointCache;
+
+    vertices.reserve(12);
+    faces.reserve(20);
+
+    { // Create initial 12 vertices
+        double distance = (1.0 + sqrt(5.0)) / 2.0;
+
+        vertices.emplace_back(glm::normalize(glm::vec3(-1, distance, 0)));
+        vertices.emplace_back(glm::normalize(glm::vec3(1, distance, 0)));
+        vertices.emplace_back(glm::normalize(glm::vec3(-1, -distance, 0)));
+        vertices.emplace_back(glm::normalize(glm::vec3(1, -distance, 0)));
+
+        vertices.emplace_back(glm::normalize(glm::vec3(0, -1, distance)));
+        vertices.emplace_back(glm::normalize(glm::vec3(0, 1, distance)));
+        vertices.emplace_back(glm::normalize(glm::vec3(0, -1, -distance)));
+        vertices.emplace_back(glm::normalize(glm::vec3(0, 1, -distance)));
+
+        vertices.emplace_back(glm::normalize(glm::vec3(distance, 0, -1)));
+        vertices.emplace_back(glm::normalize(glm::vec3(distance, 0, 1)));
+        vertices.emplace_back(glm::normalize(glm::vec3(-distance, 0, -1)));
+        vertices.emplace_back(glm::normalize(glm::vec3(-distance, 0, 1)));
+    }
+
+    { // Create 20 base triangles
+      // 5 faces around point 0
+        faces.emplace_back(0, 11, 5);
+        faces.emplace_back(0, 5, 1);
+        faces.emplace_back(0, 1, 7);
+        faces.emplace_back(0, 7, 10);
+        faces.emplace_back(0, 10, 11);
+
+        // 5 adjacent faces
+        faces.emplace_back(1, 5, 9);
+        faces.emplace_back(5, 11, 4);
+        faces.emplace_back(11, 10, 2);
+        faces.emplace_back(10, 7, 6);
+        faces.emplace_back(7, 1, 8);
+
+        // 5 faces around point 3
+        faces.emplace_back(3, 9, 4);
+        faces.emplace_back(3, 4, 2);
+        faces.emplace_back(3, 2, 6);
+        faces.emplace_back(3, 6, 8);
+        faces.emplace_back(3, 8, 9);
+
+        // 5 adjacent faces
+        faces.emplace_back(4, 9, 5);
+        faces.emplace_back(2, 4, 11);
+        faces.emplace_back(6, 2, 10);
+        faces.emplace_back(8, 6, 7);
+        faces.emplace_back(9, 8, 1);
+    }
+
+    // Refine the triangles
+    for (int i = 0; i < recursion; ++i) {
+        faces.reserve(faces.size() * 4);
+
+        auto endTri = faces.end();
+        for (auto tri = faces.begin(); tri != endTri; ++tri) {
+            // Subdivide the original triangle
+            uint32_t a = getMiddlePoint(tri->x, tri->y, middlePointCache, vertices);
+            uint32_t b = getMiddlePoint(tri->y, tri->z, middlePointCache, vertices);
+            uint32_t c = getMiddlePoint(tri->z, tri->x, middlePointCache, vertices);
+
+            faces.emplace_back(tri->x, a, c);
+            faces.emplace_back(tri->y, b, a);
+            faces.emplace_back(tri->z, c, b);
+            // The current triangle is updated, the old one has been replaced with the four
+            // subdivided ones instead
+            *tri = glm::uvec3{a, b, c};
+        }
+    }
+
+    return std::make_tuple(vertices, faces);
+}
+
+template <typename T>
+auto sphereVertexData(int recursion,
+                      uint32_t componentCount,
+                      foeVertexComponent const *pComponents,
+                      T *pData) {
+    auto [vertices, faces] = createIcoSphere(recursion);
+
+    for (auto &vertex : vertices) {
+        for (uint32_t c = 0; c < componentCount; ++c) {
+
+            switch (pComponents[c]) {
+            case foeVertexComponent::Position:
+            case foeVertexComponent::Normal:
+                *pData++ = vertex.x;
+                *pData++ = vertex.y;
+                *pData++ = vertex.z;
+                break;
+
+            case foeVertexComponent::UV:
+                // Need to calculate the UV coordinates, where X/Z determine the U, and Y determines
+                // the V
+                {
+                    constexpr float cOverPi = 1.0 / glm::pi<float>();
+                    float u = 0.5 - 0.5 * glm::atan(vertex.x, -vertex.z) * cOverPi;
+                    float v = 1.0 - acos(vertex.y) * cOverPi;
+
+                    *pData++ = u;
+                    *pData++ = v;
+                }
+                break;
+
+            case foeVertexComponent::Tangent: {
+                auto tangent = calculateTangent(vertex);
+                *pData++ = tangent.x;
+                *pData++ = tangent.y;
+                *pData++ = tangent.z;
+            } break;
+
+            case foeVertexComponent::Bitangent: {
+                auto tangent = calculateBitangent(vertex);
+                *pData++ = tangent.x;
+                *pData++ = tangent.y;
+                *pData++ = tangent.z;
+            } break;
+
+            case foeVertexComponent::Colour:
+                FOE_LOG(Model, Fatal,
+                        "Tried to generate cube vertex data with an unsupported colour component");
+                std::abort();
+            }
+        }
+    }
+}
+
+template <typename T>
+void sphereIndexData(int recursion, T offset, T *pData) {
+    auto [vertices, faces] = createIcoSphere(recursion);
+
+    for (auto &face : faces) {
+        *pData++ = offset + face.x;
+        *pData++ = offset + face.y;
+        *pData++ = offset + face.z;
+    }
+}
+
+} // namespace
+
+int foeModelIcoSphereNumVertices(int recursion) noexcept {
+    auto [vertices, faces] = createIcoSphere(recursion);
+
+    return vertices.size();
+}
+
+void foeModelIcoSphereVertexData(int recursion,
+                                 uint32_t componentCount,
+                                 foeVertexComponent const *pComponents,
+                                 float *pData) {
+    ::sphereVertexData<float>(recursion, componentCount, pComponents, pData);
+}
+
+int foeModelIcoSphereNumIndices(int recursion) noexcept {
+    auto [vertices, faces] = createIcoSphere(recursion);
+
+    return faces.size() * 3;
+}
+
+void foeModelIcoSphereIndexData16(int recursion, uint16_t offset, uint16_t *pData) {
+    ::sphereIndexData<uint16_t>(recursion, offset, pData);
+}
+
+void foeModelIcoSphereIndexData32(int recursion, uint32_t offset, uint32_t *pData) {
+    ::sphereIndexData<uint32_t>(recursion, offset, pData);
+}
