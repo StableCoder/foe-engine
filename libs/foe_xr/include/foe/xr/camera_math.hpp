3da6698c3d5f36069b123f7811cf3fe3ffe18413
George Cave <gcave@stablecoder.ca>
2021-01-06 21:07:49 -0500

    Added functions dealing with math for Xr cameras
    Projection matrices, and position/orientation conversion from
    Xr-types to glm.

---
+++ libs/foe_xr/include/foe/xr/camera_math.hpp
@@ -0,0 +1,30 @@
+/*
+    Copyright (C) 2020 George Cave.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef FOE_XR_CAMERA_HPP
+#define FOE_XR_CAMERA_HPP
+
+#include <foe/xr/export.h>
+#include <glm/glm.hpp>
+#include <openxr/openxr.h>
+
+FOE_XR_EXPORT glm::mat4 foeXrProjectionMatrix(XrFovf const &fieldOfView, float nearZ, float farZ);
+
+FOE_XR_EXPORT glm::quat foeXrPoseOrientation(XrPosef const &pose);
+
+FOE_XR_EXPORT glm::vec3 foeXrPosePosition(XrPosef const &pose);
+
+#endif // FOE_XR_CAMERA_HPP
---
+++ libs/foe_xr/src/camera_math.cpp
@@ -0,0 +1,61 @@
+/*
+    Copyright (C) 2020 George Cave.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <foe/xr/camera_math.hpp>
+
+#include <glm/ext.hpp>
+
+glm::mat4 foeXrProjectionMatrix(XrFovf const &fieldOfView, float nearZ, float farZ) {
+    float left = tan(fieldOfView.angleLeft);
+    float right = tan(fieldOfView.angleRight);
+    float up = tan(fieldOfView.angleUp);
+    float down = tan(fieldOfView.angleDown);
+
+    float width = right - left;
+    float height = up - down;
+
+    return glm::mat4{
+        // Col 1
+        2.f / width,
+        0,
+        0,
+        0,
+        // Col 2
+        0,
+        2.f / height,
+        0,
+        0,
+        // Col 3
+        (right + left) / width,
+        (up + down) / height,
+        -farZ / (farZ - nearZ),
+        -1,
+        // Col 4
+        0,
+        0,
+        -(farZ * nearZ) / (farZ - nearZ),
+        0,
+    };
+}
+
+glm::quat foeXrPoseOrientation(XrPosef const &pose) {
+    return glm::quat{pose.orientation.w * -1.f, pose.orientation.x, pose.orientation.y * -1.f,
+                     pose.orientation.z};
+}
+
+glm::vec3 foeXrPosePosition(XrPosef const &pose) {
+    return glm::vec3{pose.position.x, -pose.position.y, pose.position.z};
+}
