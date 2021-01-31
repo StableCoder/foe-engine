ae86f9e7f682821124fb87b4efef460ef53d2b2a
George Cave <gcave@stablecoder.ca>
2021-01-31 14:19:52 -0500

    Added basic infrastructure for foe_resource
    The new library will be home to management of external resources,
    typically items for graphics and audio.

---
+++ libs/foe_resource/src/error_code.cpp
@@ -0,0 +1,47 @@
+/*
+    Copyright (C) 2021 George Cave.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include "error_code.hpp"
+
+namespace {
+
+struct foeResourceErrCategory : std::error_category {
+    const char *name() const noexcept override;
+    std::string message(int ev) const override;
+};
+
+const char *foeResourceErrCategory::name() const noexcept { return "foeResourceResult"; }
+
+std::string foeResourceErrCategory::message(int ev) const {
+    switch (static_cast<foeResourceResult>(ev)) {
+    case FOE_RESOURCE_SUCCESS:
+        return "FOE_RESOURCE_SUCCESS";
+
+    default:
+        if (ev > 0)
+            return "(unrecognized positive foeResourceResult value)";
+        else
+            return "(unrecognized negative foeResourceResult value)";
+    }
+}
+
+const foeResourceErrCategory resourceErrCategory{};
+
+} // namespace
+
+std::error_code make_error_code(foeResourceResult e) {
+    return {static_cast<int>(e), resourceErrCategory};
+}
---
+++ libs/foe_resource/src/error_code.hpp
@@ -0,0 +1,33 @@
+/*
+    Copyright (C) 2021 George Cave.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef ERROR_CODE_HPP
+#define ERROR_CODE_HPP
+
+#include <system_error>
+
+enum foeResourceResult : int {
+    FOE_RESOURCE_SUCCESS = 0,
+};
+
+namespace std {
+template <>
+struct is_error_code_enum<foeResourceResult> : true_type {};
+} // namespace std
+
+std::error_code make_error_code(foeResourceResult);
+
+#endif // ERROR_CODE_HPP
