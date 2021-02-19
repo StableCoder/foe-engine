eb3b410fa3e49b00b4bfd4511bc17db3b908d4f1
George Cave <gcave@stablecoder.ca>
2021-02-18 22:59:44 -0500

    Added foe_ecs_yaml library
    Currently only dealing with importing or exporting the index generator
    type.

---
+++ libs/foe_ecs/libs/yaml/include/foe/ecs/yaml/index_generator.hpp
@@ -0,0 +1,29 @@
+/*
+    Copyright (C) 2021 George Cave.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef FOE_ECS_YAML_INDEX_GENERATOR_HPP
+#define FOE_ECS_YAML_INDEX_GENERATOR_HPP
+
+#include <foe/ecs/index_generator.hpp>
+#include <foe/ecs/yaml/export.h>
+#include <yaml-cpp/yaml.h>
+
+FOE_ECS_YAML_EXPORT void yaml_read_index_generator(YAML::Node const &node,
+                                                   foeEcsIndexGenerator &indexGenerator);
+
+FOE_ECS_YAML_EXPORT auto yaml_write_index_generator(foeEcsIndexGenerator &data) -> YAML::Node;
+
+#endif // FOE_ECS_YAML_INDEX_GENERATOR_HPP
---
+++ libs/foe_ecs/libs/yaml/src/index_generator.cpp
@@ -0,0 +1,79 @@
+/*
+    Copyright (C) 2021 George Cave.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <foe/ecs/yaml/index_generator.hpp>
+
+#include <foe/yaml/exception.hpp>
+
+void yaml_read_index_generator(YAML::Node const &node, foeEcsIndexGenerator &indexGenerator) {
+    foeIndexID nextIndex;
+    std::vector<foeIndexID> recycledIndices;
+
+    // Next Free Index
+    if (auto nextNode = node["next_free_index"]; nextNode) {
+        try {
+            nextIndex = nextNode.as<foeIndexID>();
+        } catch (...) {
+            throw foeYamlException{
+                "yaml_read_index_generator::next_free_index - Could not parse value of '" +
+                nextNode.as<std::string>() + "' to foeIndexID"};
+        }
+    } else {
+        throw foeYamlException{
+            "yaml_read_index_generator - Could not find required 'next_free_index' node"};
+    }
+
+    // Recycled Indices
+    if (auto recycledNode = node["recycled_indices"]; recycledNode) {
+        recycledIndices.reserve(recycledNode.size());
+
+        for (auto it = recycledNode.begin(); it != recycledNode.end(); ++it) {
+            try {
+                recycledIndices.emplace_back(it->as<foeIndexID>());
+            } catch (...) {
+                throw foeYamlException{
+                    "yaml_read_index_generator::recycled_indices - Could not parse value of '" +
+                    it->as<std::string>() + "' as a foeIndexID"};
+            }
+        }
+    } else {
+        throw foeYamlException{
+            "yaml_read_index_generator - Could not find required 'recycled_indices' node"};
+    }
+
+    indexGenerator.importState(nextIndex, recycledIndices);
+}
+
+auto yaml_write_index_generator(foeEcsIndexGenerator &data) -> YAML::Node {
+    YAML::Node node;
+
+    foeIndexID nextIndex;
+    std::vector<foeIndexID> recycledIndices;
+
+    data.exportState(nextIndex, recycledIndices);
+
+    // Next Free Index
+    node["next_free_index"] = nextIndex;
+
+    // Recycled Indices
+    YAML::Node recycledNode;
+    for (auto it : recycledIndices) {
+        recycledNode.push_back(it);
+    }
+    node["recycled_indices"] = node;
+
+    return node;
+}
