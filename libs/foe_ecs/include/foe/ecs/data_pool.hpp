914657ccbd1202aa8068173c63df59e1460c316b
George Cave <gcave@stablecoder.ca>
2021-05-10 18:58:25 -0400

    Added new data pool type
    Based in foe_ecs because of the use of foeId as a tying identifier and
    using the new storage types to achieve higher performance
    characteristics.
    
    As it is variadic, it also allows for splitting otherwise tied-together
    data, such as in a hot/cold pattern.

---
+++ libs/foe_ecs/include/foe/ecs/data_pool.hpp
@@ -0,0 +1,480 @@
+/*
+    Copyright (C) 2021 George Cave.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef FOE_ECS_DATA_POOL_HPP
+#define FOE_ECS_DATA_POOL_HPP
+
+#include <foe/ecs/id.hpp>
+#include <foe/storage/multi_alloc.hpp>
+
+#include <cstring>
+#include <map>
+#include <mutex>
+#include <shared_mutex>
+#include <vector>
+
+template <typename... Components>
+class foeDataPool {
+    /// Common
+  public:
+    ~foeDataPool() {
+        multiDelete(&mMainStorage, 0, size());
+        clearRemoved();
+    }
+
+  private:
+    using TupleType = std::tuple<foeId, Components...>;
+    using PoolStore = foeMultiAllocStorage<foeId, Components...>;
+
+    template <int Index = 0>
+    void singleEmplace(PoolStore *pDst,
+                       size_t dstOffset,
+                       foeId id,
+                       std::tuple<Components...> &&components) {
+        auto *pDstData = pDst->template get<Index>() + dstOffset;
+
+        if constexpr (Index == 0) {
+            // Specifically for the ID
+            *pDstData = id;
+        } else {
+            // Components
+            using IndexType = typename std::tuple_element<Index, TupleType>::type;
+
+            new (pDstData) IndexType{std::move(std::get<Index - 1>(components))};
+        }
+
+        if constexpr (Index != sizeof...(Components)) {
+            singleEmplace<Index + 1>(pDst, dstOffset, id, std::move(components));
+        }
+    }
+
+    template <int Index = 0>
+    void multiMove(
+        PoolStore *pSrc, size_t startOffset, size_t count, PoolStore *pDst, size_t dstOffset) {
+        using IndexType = typename std::tuple_element<Index, TupleType>::type;
+
+        auto *pData = pSrc->template get<Index>() + startOffset;
+        auto *pDstData = pDst->template get<Index>() + dstOffset;
+
+        if constexpr (std::is_trivially_copyable<IndexType>::value) {
+            memmove(pDstData, pData, count * sizeof(foeId));
+        } else {
+            // Components
+            auto *pDataEnd = pData + count;
+
+            while (pData != pDataEnd) {
+                new (pDstData++) IndexType{std::move(*pData++)};
+            }
+        }
+
+        // If not the last component type, go deeper
+        if constexpr (Index != sizeof...(Components)) {
+            multiMove<Index + 1>(pSrc, startOffset, count, pDst, dstOffset);
+        }
+    }
+
+    template <int Index = 0>
+    void multiDelete(PoolStore *pStore, size_t offset, size_t count) {
+        using IndexType = typename std::tuple_element<Index, TupleType>::type;
+
+        if constexpr (!std::is_trivially_destructible<IndexType>::value) {
+            auto *pData = pStore->template get<Index>() + offset;
+            auto *const pDataEnd = pData + count;
+
+            while (pData != pDataEnd) {
+                (pData++)->~IndexType();
+            }
+        }
+
+        // If not the last component type, go deeper
+        if constexpr (Index != sizeof...(Components)) {
+            multiDelete<Index + 1>(pStore, offset, count);
+        }
+    }
+
+    /// Regular
+  public:
+    void maintenance() {
+        clearRemoved();
+        clearInserted();
+
+        removePass();
+        insertPass();
+    }
+
+    size_t capacity() const noexcept { return mMainStorage.capacity(); }
+    size_t size() const noexcept { return mStored; }
+
+    bool exist(foeId id) const noexcept { return find(id) != size(); }
+
+    size_t sequential_search(foeId id) const noexcept {
+        auto it = begin();
+        auto const endIt = end();
+
+        while (it != endIt) {
+            if (*it == id) {
+                return it - begin();
+            }
+            ++it;
+        }
+
+        return size();
+    }
+
+    size_t binary_search(foeId id) const noexcept {
+        auto searchIt = std::lower_bound(begin(), end(), id);
+        if (searchIt != end() && *searchIt == id) {
+            return searchIt - begin();
+        }
+        return size();
+    }
+
+    size_t find(foeId id) const noexcept {
+        auto searchIt = std::find(begin(), end(), id);
+        return searchIt - begin();
+    }
+
+    template <int Index = 0>
+    auto begin() noexcept -> typename std::tuple_element<Index, TupleType>::type * {
+        return static_cast<typename std::tuple_element<Index, TupleType>::type *>(
+            mMainStorage.template get<Index>());
+    }
+
+    template <int Index = 0>
+    auto begin() const noexcept -> typename std::tuple_element<Index, TupleType>::type const * {
+        return static_cast<typename std::tuple_element<Index, TupleType>::type const *>(
+            mMainStorage.template get<Index>());
+    }
+
+    template <int Index = 0>
+    auto cbegin() const noexcept -> typename std::tuple_element<Index, TupleType>::type const * {
+        return static_cast<typename std::tuple_element<Index, TupleType>::type const *>(
+            mMainStorage.template get<Index>());
+    }
+
+    template <int Index = 0>
+    auto end() noexcept -> typename std::tuple_element<Index, TupleType>::type * {
+        return static_cast<typename std::tuple_element<Index, TupleType>::type *>(
+            mMainStorage.template get<Index>() + mStored);
+    }
+
+    template <int Index = 0>
+    auto end() const noexcept -> typename std::tuple_element<Index, TupleType>::type const * {
+        return static_cast<typename std::tuple_element<Index, TupleType>::type const *>(
+            mMainStorage.template get<Index>() + mStored);
+    }
+
+    template <int Index = 0>
+    auto cend() const noexcept -> typename std::tuple_element<Index, TupleType>::type const * {
+        return static_cast<typename std::tuple_element<Index, TupleType>::type const *>(
+            mMainStorage.template get<Index>() + mStored);
+    }
+
+  private:
+    size_t mExpansionRate = 8192;
+
+    PoolStore mMainStorage;
+    size_t mStored{0};
+
+    /// Insertion
+  public:
+    void insert(foeId id, Components &&...components) {
+        mToInsertSync.lock();
+
+        mToInsert.emplace_back(id, std::make_tuple(std::move(components)...));
+
+        mToInsertSync.unlock();
+    }
+
+    size_t inserted() { return mInsertedOffsets.size(); };
+
+    auto inbegin() noexcept { return mInsertedOffsets.begin(); }
+    auto inbegin() const noexcept { return mInsertedOffsets.begin(); }
+    auto cinbegin() const noexcept { return mInsertedOffsets.cbegin(); }
+
+    auto inend() noexcept { return mInsertedOffsets.end(); }
+    auto inend() const noexcept { return mInsertedOffsets.end(); }
+    auto cinend() const noexcept { return mInsertedOffsets.cend(); }
+
+  private:
+    void clearInserted() { mInsertedOffsets.clear(); }
+    void insertPass();
+
+    std::mutex mToInsertSync;
+    std::vector<std::tuple<foeId, std::tuple<Components...>>> mToInsert;
+
+    std::vector<size_t> mInsertedOffsets;
+
+    /// Removal
+  public:
+    void remove(foeId id) {
+        mToRemoveSync.lock();
+        mToRemove.emplace_back(id);
+        mToRemoveSync.unlock();
+    }
+
+    size_t removed() const noexcept { return mRemoved; }
+
+    size_t rm_sequential_search(foeId id) const noexcept {
+        auto it = rmbegin();
+        auto const endIt = rmend();
+
+        while (it != endIt) {
+            if (*it == id) {
+                return it - rmbegin();
+            }
+            ++it;
+        }
+
+        return removed();
+    }
+
+    size_t rm_binary_search(foeId id) const noexcept {
+        auto searchIt = std::lower_bound(rmbegin(), rmend(), id);
+        if (searchIt != rmend() && *searchIt == id) {
+            return searchIt - rmbegin();
+        }
+        return removed();
+    }
+
+    size_t rm_find(foeId id) const noexcept {
+        auto searchIt = std::find(rmbegin(), rmend(), id);
+        return searchIt - rmbegin();
+    }
+
+    template <int Index = 0>
+    auto rmbegin() noexcept -> typename std::tuple_element<Index, TupleType>::type * {
+        return static_cast<typename std::tuple_element<Index, TupleType>::type *>(
+            mRemovedStore.template get<Index>());
+    }
+
+    template <int Index = 0>
+    auto rmbegin() const noexcept -> typename std::tuple_element<Index, TupleType>::type const * {
+        return static_cast<typename std::tuple_element<Index, TupleType>::type const *>(
+            mRemovedStore.template get<Index>());
+    }
+
+    template <int Index = 0>
+    auto crmbegin() const noexcept -> typename std::tuple_element<Index, TupleType>::type const * {
+        return static_cast<typename std::tuple_element<Index, TupleType>::type const *>(
+            mRemovedStore.template get<Index>());
+    }
+
+    template <int Index = 0>
+    auto rmend() noexcept -> typename std::tuple_element<Index, TupleType>::type * {
+        return static_cast<typename std::tuple_element<Index, TupleType>::type *>(
+            mRemovedStore.template get<Index>() + mRemoved);
+    }
+
+    template <int Index = 0>
+    auto rmend() const noexcept -> typename std::tuple_element<Index, TupleType>::type const * {
+        return static_cast<typename std::tuple_element<Index, TupleType>::type const *>(
+            mRemovedStore.template get<Index>() + mRemoved);
+    }
+
+    template <int Index = 0>
+    auto crmend() const noexcept -> typename std::tuple_element<Index, TupleType>::type const * {
+        return static_cast<typename std::tuple_element<Index, TupleType>::type const *>(
+            mRemovedStore.template get<Index>() + mRemoved);
+    }
+
+  private:
+    void clearRemoved() {
+        multiDelete(&mRemovedStore, 0, mRemoved);
+        mRemoved = 0;
+    }
+    void removePass();
+
+    // To be removed stuff
+    std::mutex mToRemoveSync;
+    std::vector<foeId> mToRemove;
+
+    // Stuff removed last maintenance cycle
+    PoolStore mRemovedStore;
+    size_t mRemoved{0};
+};
+
+template <typename... Components>
+void foeDataPool<Components...>::insertPass() {
+    mToInsertSync.lock();
+    auto toInsert = std::move(mToInsert);
+    mToInsertSync.unlock();
+
+    // If there's nothing to insert, leave
+    if (toInsert.empty())
+        return;
+
+    std::sort(toInsert.begin(), toInsert.end(),
+              [](auto const &a, auto const &b) { return std::get<0>(a) < std::get<0>(b); });
+    toInsert.erase(
+        std::unique(toInsert.begin(), toInsert.end(),
+                    [](auto const &a, auto const &b) { return std::get<0>(a) == std::get<0>(b); }),
+        toInsert.end());
+
+    mInsertedOffsets.reserve(toInsert.size());
+
+    std::vector<std::pair<
+        typename std::vector<std::tuple<foeId, std::tuple<Components...>>>::iterator, size_t>>
+        srcDstOffsets;
+    srcDstOffsets.reserve(toInsert.size());
+
+    {
+        // Iterators to the insertion map items
+        auto inIt = toInsert.begin();
+        auto const inEndIt = toInsert.end();
+        // Pointer/Iterator to the foeId's of items already in regular storage
+        auto *const pBeginId = mMainStorage.get();
+        auto *const pEndId = mMainStorage.get() + mStored;
+        auto *pId = pBeginId;
+
+        while (inIt != inEndIt) {
+            // Find the next destination placement
+            while (pId != pEndId && *pId < std::get<0>(*inIt)) {
+                ++pId;
+            }
+
+            // Same values, item already in regular list, skip the insertion of this element
+            if (pId != pEndId && *pId == std::get<0>(*inIt)) {
+                ++inIt;
+                continue;
+            }
+
+            // Worry about duplicates? Not for map
+
+            // At this point, it is not already in storage, and is unique, so it is to be added
+            srcDstOffsets.emplace_back(inIt, pId - pBeginId);
+
+            ++inIt;
+        }
+    }
+
+    // If nothing to be inserted, just skip out
+    if (srcDstOffsets.empty()) {
+        return;
+    }
+
+    // Resize main storage if necessary
+    if (auto newMinSize = mStored + srcDstOffsets.size(); mMainStorage.capacity() < newMinSize) {
+        newMinSize = ((newMinSize / mExpansionRate) + 1) * mExpansionRate;
+        PoolStore newStore{newMinSize};
+
+        multiMove(&mMainStorage, 0, mStored, &newStore, 0);
+
+        mMainStorage = std::move(newStore);
+    }
+
+    size_t lastMovedOldItem = mStored;
+    size_t accumulatedDistance = srcDstOffsets.size();
+
+    for (auto srcDstIt = srcDstOffsets.rbegin(); srcDstIt != srcDstOffsets.rend(); ++srcDstIt) {
+        size_t givenOffset = srcDstIt->second;
+        srcDstIt->second += accumulatedDistance - 1;
+
+        // If the item is being placed before some old objects, then we need to shift them up
+        if (givenOffset < lastMovedOldItem) {
+            size_t src = givenOffset;
+            size_t dst = givenOffset + accumulatedDistance;
+            size_t numMove = lastMovedOldItem - givenOffset;
+
+            multiMove(&mMainStorage, src, numMove, &mMainStorage, dst);
+
+            lastMovedOldItem = givenOffset;
+        }
+
+        // Everything has been shifted out of the way, place the entry now
+        singleEmplace(&mMainStorage, srcDstIt->second, std::get<0>(*srcDstIt->first),
+                      std::move(std::get<1>(*srcDstIt->first)));
+
+        // We added an item, decrement the accumulated distance
+        --accumulatedDistance;
+
+        // Add the aded ID to the inserted list
+        mInsertedOffsets.emplace_back(srcDstIt->second);
+    }
+
+    // Set the new number of held items
+    mStored += srcDstOffsets.size();
+
+    // Reverse the inserted lists to be in ascending order
+    std::reverse(mInsertedOffsets.begin(), mInsertedOffsets.end());
+}
+
+template <typename... Components>
+void foeDataPool<Components...>::removePass() {
+    mToRemoveSync.lock();
+    auto toRemove = std::move(mToRemove);
+    mToRemoveSync.unlock();
+
+    // If there's nothing to remove, leave
+    if (toRemove.empty())
+        return;
+
+    // Sort the IDs to be in-order
+    std::sort(toRemove.begin(), toRemove.end());
+
+    // Prepare removed storage for new items, make sure has enough capacity
+    if (mRemovedStore.capacity() < toRemove.size()) {
+        mRemovedStore = std::move(PoolStore{toRemove.size()});
+    }
+
+    auto *const pIdStart = mMainStorage.template get<0>();
+    auto *const pIdEnd = pIdStart + mStored;
+    auto *pId = pIdStart;
+
+    auto rmIt = toRemove.cbegin();
+    auto const rmItEnd = toRemove.cend();
+
+    size_t lastMovedObject = 0;
+    while (pId != pIdEnd && rmIt != rmItEnd) {
+        // If we haven't found the next item to remove yet, continue the search
+        if (*pId < *rmIt) {
+            ++pId;
+            continue;
+        }
+
+        if (*pId == *rmIt) {
+            // Move the removed entry out
+            auto const offset = pId - pIdStart;
+            multiMove(&mMainStorage, offset, 1, &mRemovedStore, mRemoved);
+
+            // Move any regular object that need to be shifted down.
+            if (lastMovedObject != 0) {
+                multiMove(&mMainStorage, lastMovedObject, offset - lastMovedObject, &mMainStorage,
+                          lastMovedObject - mRemoved);
+            }
+
+            ++pId;
+            ++rmIt;
+            // We removed another and it's used in-loop, increment
+            ++mRemoved;
+            lastMovedObject = offset + 1;
+        } else {
+            // The ID is larger than the ID to remove, so the ID isn't in the pool, so skip this
+            // remove request
+            ++rmIt;
+        }
+    }
+
+    if (lastMovedObject != 0) {
+        multiMove(&mMainStorage, lastMovedObject, mStored - lastMovedObject, &mMainStorage,
+                  lastMovedObject - mRemoved);
+    }
+
+    // Finally, decrement the total stored by the number we just removed.
+    mStored -= mRemoved;
+}
+
+#endif // FOE_ECS_DATA_POOL_HPP
