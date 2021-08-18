c74a67eb12a23bcda6f052cb8a3b8fea167bafc1
George Cave <gcave@stablecoder.ca>
2021-08-17 23:34:41 -0400

    Added bringup-data-sets as a submodule
    It was added as a weird detached repository instead of a submodule to
    try to lessen accidental submodule initializations as it would
    eventually contain some heavier binary data.
    
    However it's just easier to maintain as a submodule, so it is being
    re-added as one.

--- data/bringup_data_sets.ps1
+++
@@ -1,5 +0,0 @@
-$COMMIT=$(cat bringup_data_sets_sha.txt)
-
-git clone https://git.stabletec.com/foe/bringup-data-sets.git
-cd bringup-data-sets
-git checkout $COMMIT
--- data/bringup_data_sets.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-COMMIT=$(cat bringup_data_sets_sha.txt)
-
-git clone https://git.stabletec.com/foe/bringup-data-sets.git
-cd bringup-data-sets
-git checkout $COMMIT
