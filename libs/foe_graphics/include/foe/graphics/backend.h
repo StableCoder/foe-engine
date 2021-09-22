6fd61a10814d7976ce98c6b2654d3c438c1a5200
George Cave <gcave@stablecoder.ca>
2021-09-21 21:10:29 -0400

    Fixed static assert for GCC
    GCC won't use the C-style assert in C++, so need to specify the assert
    call for both languages.

--- libs/foe_graphics/include/foe/graphics/backend.h
+++ libs/foe_graphics/include/foe/graphics/backend.h
@@ -29,8 +29,13 @@ struct foeGfxVersion {
     unsigned int patch : 12;
 };
 
+#ifdef __cplusplus
+static_assert(sizeof(struct foeGfxVersion) == 4,
+              "foeGfxVersion should be 4 bytes in size (uint32_t)");
+#else
 _Static_assert(sizeof(struct foeGfxVersion) == 4,
                "foeGfxVersion should be 4 bytes in size (uint32_t)");
+#endif
 
 FOE_GFX_EXPORT char const *foeGfxBackendName();
 
