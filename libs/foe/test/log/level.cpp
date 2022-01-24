784a04ebc662eaec0c101e4398b434879014c651
George Cave <gcave@stablecoder.ca>
2022-01-24 00:00:19 -0500

    Modified log level checks
    Changed from require to check, and added check for the default unknown
    case.

--- libs/foe/test/log/level.cpp
+++ libs/foe/test/log/level.cpp
@@ -18,9 +18,11 @@
 #include <foe/log/level.hpp>
 
 TEST_CASE("foeLogLevel - to_string checks", "[foe][log]") {
-    REQUIRE(std::to_string(foeLogLevel::Fatal) == "Fatal");
-    REQUIRE(std::to_string(foeLogLevel::Error) == "Error");
-    REQUIRE(std::to_string(foeLogLevel::Warning) == "Warning");
-    REQUIRE(std::to_string(foeLogLevel::Info) == "Info");
-    REQUIRE(std::to_string(foeLogLevel::Verbose) == "Verbose");
+    CHECK(std::to_string(foeLogLevel::Fatal) == "Fatal");
+    CHECK(std::to_string(foeLogLevel::Error) == "Error");
+    CHECK(std::to_string(foeLogLevel::Warning) == "Warning");
+    CHECK(std::to_string(foeLogLevel::Info) == "Info");
+    CHECK(std::to_string(foeLogLevel::Verbose) == "Verbose");
+
+    CHECK(std::to_string(static_cast<foeLogLevel>(999999)) == "Unknown Level");
 }
