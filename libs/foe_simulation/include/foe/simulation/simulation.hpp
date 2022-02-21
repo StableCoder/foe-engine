d2e795e7b3e43c71290cfbece9df963308cbd516
George Cave <gcave@stablecoder.ca>
2022-02-21 13:37:30 -0500

    Removed asyncTaskFn from Simulation init info
    Other than being set for simulation initialization, it is not yet used
    by anything.

--- libs/foe_simulation/include/foe/simulation/simulation.hpp
+++ libs/foe_simulation/include/foe/simulation/simulation.hpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2021 George Cave.
+    Copyright (C) 2021-2022 George Cave.
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -50,7 +50,6 @@ struct foeSimulationCreateInfo {
 struct foeSimulationInitInfo {
     foeGfxSession gfxSession;
     std::function<std::filesystem::path(std::filesystem::path)> externalFileSearchFn;
-    std::function<void(std::function<void()>)> asyncJobFn;
 };
 
 struct foeSimulationLoaderData {
--- src/application.cpp
+++ src/application.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2021 George Cave.
+    Copyright (C) 2021-2022 George Cave.
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -448,7 +448,6 @@ auto Application::initialize(int argc, char **argv) -> std::tuple<bool, int> {
             .gfxSession = gfxSession,
             .externalFileSearchFn = std::bind(&foeGroupData::findExternalFile,
                                               &pSimulationSet->groupData, std::placeholders::_1),
-            .asyncJobFn = asyncTaskFunc,
         };
         foeInitializeSimulation(pSimulationSet, &simInitInfo);
     }
