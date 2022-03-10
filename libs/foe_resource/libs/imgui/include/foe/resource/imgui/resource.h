ff2b1537f661ebf55b5d99be71a4bf0046d63cc9
George Cave <gcave@stablecoder.ca>
2022-03-10 17:46:09 -0500

    Add imgui sub-library for foe_resource
    Resources will need to have resource information be presentable in UI's.
    This accomplishes that for foeResource with ImGui.

---
+++ libs/foe_resource/libs/imgui/include/foe/resource/imgui/resource.h
@@ -0,0 +1,33 @@
+/*
+    Copyright (C) 2022 George Cave.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef FOE_RESOURCE_IMGUI_RESOURCE_H
+#define FOE_RESOURCE_IMGUI_RESOURCE_H
+
+#include <foe/resource/imgui/export.h>
+#include <foe/resource/resource.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+FOE_RES_IMGUI_EXPORT void imgui_renderResource(foeResource resource);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // FOE_RESOURCE_IMGUI_RESOURCE_H
---
+++ libs/foe_resource/libs/imgui/src/resource.cpp
@@ -0,0 +1,40 @@
+/*
+    Copyright (C) 2022 George Cave.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <foe/resource/imgui/resource.h>
+
+#include <imgui.h>
+
+extern "C" void imgui_renderResource(foeResource resource) {
+    // Is Loading
+    if (foeResourceGetIsLoading(resource))
+        ImGui::Text("Is Loading: true");
+    else
+        ImGui::Text("Is Loading: false");
+
+    // State
+    auto const state = foeResourceGetState(resource);
+    if (state == foeResourceLoadState::Unloaded)
+        ImGui::Text("LoadState: unloaded");
+    else if (state == foeResourceLoadState::Loaded)
+        ImGui::Text("LoadState: loaded");
+    else if (state == foeResourceLoadState::Failed)
+        ImGui::Text("LoadState: failed");
+
+    // Counts
+    ImGui::Text("Ref Count: %u", foeResourceGetRefCount(resource));
+    ImGui::Text("Use Count: %u", foeResourceGetUseCount(resource));
+}
