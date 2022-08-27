236c4f791a8e1367d1fa9b5ea74f9610f53cff99
George Cave <gcave@stablecoder.ca>
2022-08-26 17:20:19 -0700

    Add more tests for Yaml libraries
    C header compatibility and result code tests for graphics_resource,
    physics and posiiton Yaml libraries.

---
+++ libs/foe_physics/libs/yaml/test/c_header_compatibility.c
@@ -0,0 +1,7 @@
+// Copyright (C) 2022 George Cave.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <foe/physics/yaml/export_registration.h>
+#include <foe/physics/yaml/import_registration.h>
+#include <foe/physics/yaml/result.h>
---
+++ libs/foe_position/libs/yaml/test/c_header_compatibility.c
@@ -0,0 +1,7 @@
+// Copyright (C) 2022 George Cave.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <foe/position/yaml/export_registration.h>
+#include <foe/position/yaml/import_registration.h>
+#include <foe/position/yaml/result.h>
