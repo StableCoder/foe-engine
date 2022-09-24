dd8fd585cc0971b38ecc5d05cfb1f0d605802852
George Cave <gcave@stablecoder.ca>
2022-09-23 22:46:48 -0400

    Tests for ManagedMemory directly

---
+++ libs/foe/test/managed_memory.cpp
@@ -0,0 +1,86 @@
+// Copyright (C) 2022 George Cave.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <catch.hpp>
+#include <foe/managed_memory.h>
+#include <foe/result.h>
+
+constexpr size_t cDataCount = 2048;
+
+TEST_CASE("ManagedMemory - No metadata/cleanup") {
+    foeManagedMemory managedMemory = FOE_NULL_HANDLE;
+    foeResultSet result;
+
+    uint16_t buffer[cDataCount];
+    for (int i = 0; i < cDataCount; ++i) {
+        buffer[i] = i;
+    }
+
+    result = foeCreateManagedMemory(buffer, sizeof(uint16_t) * cDataCount, nullptr, nullptr, 0,
+                                    &managedMemory);
+    CHECK(result.value == FOE_SUCCESS);
+    REQUIRE(managedMemory != FOE_NULL_HANDLE);
+    CHECK(foeManagedMemoryGetUse(managedMemory) == 1);
+
+    uint16_t *ptr = nullptr;
+    uint32_t size = 0;
+
+    foeManagedMemoryGetData(managedMemory, (void **)&ptr, &size);
+    REQUIRE(ptr == buffer);
+    REQUIRE(size == cDataCount * sizeof(uint16_t));
+
+    for (uint16_t i = 0; i < cDataCount; ++i) {
+        if (ptr[i] != i)
+            CHECK(ptr[i] == i);
+    }
+
+    CHECK(foeManagedMemoryIncrementUse(managedMemory) == 2);
+    CHECK(foeManagedMemoryGetUse(managedMemory) == 2);
+    CHECK(foeManagedMemoryDecrementUse(managedMemory) == 1);
+    CHECK(foeManagedMemoryGetUse(managedMemory) == 1);
+
+    REQUIRE(foeManagedMemoryDecrementUse(managedMemory) == 0);
+}
+
+TEST_CASE("ManagedMemory - metadata and cleanup") {
+    foeManagedMemory managedMemory = FOE_NULL_HANDLE;
+    foeResultSet result;
+
+    uint16_t *buffer = new uint16_t[cDataCount];
+    for (int i = 0; i < cDataCount; ++i) {
+        buffer[i] = i;
+    }
+
+    auto cleanupFn = [](void *pData, uint32_t, void *pMetadata) {
+        uint16_t **ppData = (uint16_t **)pMetadata;
+        CHECK(*ppData == pData);
+
+        delete[](uint16_t *) pData;
+    };
+
+    result = foeCreateManagedMemory(buffer, sizeof(uint16_t) * cDataCount, cleanupFn, &buffer,
+                                    sizeof(uint16_t *), &managedMemory);
+    CHECK(result.value == FOE_SUCCESS);
+    REQUIRE(managedMemory != FOE_NULL_HANDLE);
+    CHECK(foeManagedMemoryGetUse(managedMemory) == 1);
+
+    uint16_t *ptr = nullptr;
+    uint32_t size = 0;
+
+    foeManagedMemoryGetData(managedMemory, (void **)&ptr, &size);
+    REQUIRE(ptr == buffer);
+    REQUIRE(size == cDataCount * sizeof(uint16_t));
+
+    for (uint16_t i = 0; i < cDataCount; ++i) {
+        if (ptr[i] != i)
+            CHECK(ptr[i] == i);
+    }
+
+    CHECK(foeManagedMemoryIncrementUse(managedMemory) == 2);
+    CHECK(foeManagedMemoryGetUse(managedMemory) == 2);
+    CHECK(foeManagedMemoryDecrementUse(managedMemory) == 1);
+    CHECK(foeManagedMemoryGetUse(managedMemory) == 1);
+
+    REQUIRE(foeManagedMemoryDecrementUse(managedMemory) == 0);
+}
