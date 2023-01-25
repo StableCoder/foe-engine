27d32df3d175c056b0d158e092ef18e54ffee644
George Cave <gcave@stablecoder.ca>
2023-01-24 23:27:04 -0500

    Add missing ECS headers to C-compatibility test

--- libs/foe_ecs/test/c_header_compatibility.c
+++ libs/foe_ecs/test/c_header_compatibility.c
@@ -1,7 +1,9 @@
-// Copyright (C) 2022 George Cave.
+// Copyright (C) 2022-2023 George Cave.
 //
 // SPDX-License-Identifier: Apache-2.0
 
+#include <foe/ecs/binary.h>
+#include <foe/ecs/component_pool.h>
 #include <foe/ecs/group_translator.h>
 #include <foe/ecs/id.h>
 #include <foe/ecs/indexes.h>
