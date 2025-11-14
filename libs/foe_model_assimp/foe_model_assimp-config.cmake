8376bb1adf32d45909d82cde4be55a8444ffadbe
George Cave <gcave@stablecoder.ca>
2025-11-14 07:22:14 -0500

    Fix foe_model_assimp export missing dependency
    Need topull in the assimp dependency when exported.

--- libs/foe_model_assimp/foe_model_assimp-config.cmake
+++ libs/foe_model_assimp/foe_model_assimp-config.cmake
@@ -3,6 +3,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
 include(CMakeFindDependencyMacro)
+if(NOT UNIX)
+  find_dependency(assimp REQUIRED)
+endif()
 find_dependency(foe REQUIRED COMPONENTS model)
 
 include(${CMAKE_CURRENT_LIST_DIR}/foe_model_assimp.cmake)
